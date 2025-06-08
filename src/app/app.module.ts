0d72b7decb3a069e4922b0d6ef418466b1253c29
João Pedro Insfran <jpinsfran@gmail.com>
2025-06-08 01:19:36 -0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AngularFireModule} from '@angular/fire/compat'
+import { environment } from './environments/environment';
+import { AngularFireAuthModule} from '@angular/fire/compat/auth'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment - {
+    producition: false,
+    firebase: {
+        apiKey: "AIzaSyADBVrzCRqKQa4cPAwxRxpS0VeCxpSDCus",
+        authDomain: "asimovies-e16ce.firebaseapp.com",
+        projectId: "asimovies-e16ce",
+        storageBucket: "asimovies-e16ce.firebasestorage.app",
+        messagingSenderId: "680504441085",
+        appId: "1:680504441085:web:f5aa2c2d93d744d452e694",
+        measurementId: "G-Z4865JHEYM"
+    }
+}
